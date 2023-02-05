05e83ec7aad289f0cc9f1e230d73591735e90487
Brian Takita <brian.takita@gmail.com>
2023-02-05 18:20:49 -0500

    @cogov/www: root: Title: CoGov title

--- apps/www/src/root.tsx
+++ apps/www/src/root.tsx
@@ -35,7 +35,7 @@ export default function Root() {
 	return (
 		<Html lang="en">
       <Head>
-        <Title>SolidStart - With TailwindCSS</Title>
+        <Title>{title}</Title>
         <Meta charset="utf-8"/>
         <Meta name="viewport" content="width=device-width, initial-scale=1"/>
         <Meta name="theme-color" content="#333333"/>
