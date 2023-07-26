1a882d3e2b11613c273a9d80cba0344dcc8cf1a1
Brian Takita <brian.takita@gmail.com>
2023-07-26 08:10:45 -0400

    Revert "@cogov/dev:"
    This reverts commit e3e7f8d79de57fff97c071396341b4f0845dc167.

--- bin/cogov-www--docker-compose--rebuild.sh
+++ bin/cogov-www--docker-compose--rebuild.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
 cogov--docker-compose--build
-# shellcheck disable=SC2039
-declare -i COUNT
-# shellcheck disable=SC2039
-declare -i SCALE
-COUNT=$(echo "$RUNNING_CONTAINER_ID" | awk '{print NF}')
-SCALE="(( $COUNT + 1 ))"
-docker compose -p cogov -f d.cogov.docker-compose.yml --project-directory . up -d --scale www="$SCALE" --force-recreate --no-deps
-if [ -n "$RUNNING_CONTAINER_ID" ]; then
-	sleep 5
-	docker container stop "$RUNNING_CONTAINER_ID"
-fi
+cogov-www--docker-compose--restart
 docker system prune -f
