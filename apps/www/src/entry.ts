6ef2231b5ec9ff327aff0a3f9b7789fc4d53f8a1
Brian Takita <brian.takita@gmail.com>
2023-07-26 07:19:33 -0400

    @censible/www: entry: docker: fix: fastify root

--- apps/www/src/entry.ts
+++ apps/www/src/entry.ts
@@ -1,12 +1,11 @@
 import fastifyMiddie from '@fastify/middie'
 import fastifyStatic from '@fastify/static'
 import Fastify from 'fastify'
-import { fileURLToPath } from 'node:url'
 const entry_path = './dist/server/entry.mjs'
 const app = Fastify({ logger: true })
 await app
 	.register(fastifyStatic, {
-		root: fileURLToPath(new URL('./dist/client', import.meta.url)),
+		root: './dist/client',
 	})
 	.register(fastifyMiddie)
 app.use(await import(entry_path))
