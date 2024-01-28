50f05607dfb6ddc7fce7b679591adcf739e98d5a
Brian Takita <brian.takita@gmail.com>
2024-01-28 18:40:26 -0500

    update dependencies

--- lib/deploy/src/deploy__env.ts
+++ lib/deploy/src/deploy__env.ts
@@ -1,5 +1,5 @@
-import { import_meta_env_, type ImportMetaEnv } from 'ctx-core/env'
+import { import_meta_env_, type import_meta_env_T } from 'ctx-core/env'
 export const deploy__env:deploy__env_T = import_meta_env_()
-export interface deploy__env_T extends ImportMetaEnv {
+export interface deploy__env_T extends import_meta_env_T {
 	COGOVME_PORT:string
 }
