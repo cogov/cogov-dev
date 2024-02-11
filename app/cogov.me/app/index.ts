09701f0d188672b7f4b50e925cae46c8bda31b58
Brian Takita <brian.takita@gmail.com>
2024-02-11 05:52:29 -0500

    cogov.me: move src files into root directory

--- app/cogov.me/app/index.ts
+++ app/cogov.me/app/index.ts
@@ -1,9 +1,16 @@
 import { import_meta_env_ } from 'ctx-core/env'
 import { Elysia } from 'elysia'
-import { dirname, join, resolve } from 'path'
 import { relement__use } from 'relementjs'
 import { server__relement } from 'relementjs/server'
-import { app_ctx, compression_middleware_, cwd__set, is_prod_, port__set, static_middleware_ } from 'relysjs/server'
+import {
+	app_ctx,
+	compression_middleware_,
+	cwd__set,
+	is_prod_,
+	port__set,
+	src_path__set,
+	static_middleware_
+} from 'relysjs/server'
 export default async function app__start() {
 	config__init()
 	return new Elysia()
@@ -24,6 +31,7 @@ export default async function app__start() {
 export function config__init() {
 	const port = parseInt(import_meta_env_().COGOVME_PORT) || 4020
 	port__set(app_ctx, port)
-	cwd__set(app_ctx, resolve(join(dirname(new URL(import.meta.url).pathname), '../..')))
+	cwd__set(app_ctx, process.cwd())
+	src_path__set(app_ctx, process.cwd())
 	relement__use(server__relement)
 }
